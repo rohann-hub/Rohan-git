2fff00e35fa76fe8f8c02b756630ce5f6559c9d0
rohann <roh4nn001@gmail.com>
2024-12-13 17:44:21 +0530

    Generate _password.py

--- Generate _password.py
+++ Generate _password.py
@@ -1,3 +1,5 @@
+## Generate your password
+
 import secrets
 import string
 
@@ -11,3 +13,14 @@ def generate_password(length = 12):
                    for _ in range(length))
 password = generate_password()
 print("your new password: {password}")
+
+
+## HIDE you password 
+
+import getpass
+
+username = input("Enter your username: ")
+password = getpass.getpass("Enter your password: ")
+
+print(f"Username: {username }")
+print(f"password : {password}")
